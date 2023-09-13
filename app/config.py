f0a15f3d10eb00cb5578aeded9dedeb34cbd5fa9
Sergey Kievskiy <knhel7@mail.ru>
2023-09-13 09:46:23 +0300

    Init upscale

--- app/config.py
+++ app/config.py
@@ -3,3 +3,11 @@ import os
 CELERY_BROKER = os.getenv("CELERY_BROKER")
 CELERY_BACKEND = os.getenv("CELERY_BACKEND")
 
+CURRENT_PATH = os.getcwd()
+
+ML_PATH = os.path.join(CURRENT_PATH, os.getenv("ML_PACKAGE"))
+ML_OBJECTS = os.path.join(ML_PATH, os.getenv("ML_OBJECTS_FOLDER"))
+ML_RESULTS = os.path.join(ML_PATH, os.getenv("ML_RESULTS_FOLDER"))
+ML_MODEL = os.path.join(ML_PATH,
+                        os.getenv("ML_MODELS_FOLDER"),
+                        os.getenv("ML_CURRENT_MODEL_NAME"))
--- app/upscale/upscale.py
+++ app/upscale/upscale.py
@@ -1,27 +1,59 @@
+from functools import lru_cache
+
 import cv2
 from cv2 import dnn_superres
 
+from functools import lru_cache
 
-def upscale(input_path: str,
-            output_path: str,
-            model_path: str = 'models/EDSR_x2.pb') -> None:
+import os
+from app.config import ML_OBJECTS, ML_RESULTS, ML_MODEL
+
+
+@lru_cache
+def get_model(model_path: str):
     """
-    :param input_path: путь к изображению для апскейла
-    :param output_path:  путь к выходному файлу
     :param model_path: путь к ИИ модели
     :return:
     """
-
-    scaler = dnn_superres.DnnSuperResImpl_create()
+    scaler = dnn_superres.DnnSuperResImpl.create()
     scaler.readModel(model_path)
-    scaler.setModel("edsr", 2)
-    image = cv2.imread(input_path)
+    scaler.setModel('edsr', 2)
+    return scaler
+
+
+def upscale(input_path: str, output_path: str,
+            model_path: str = ML_MODEL) -> str:
+    """
+        :param input_path: путь к изображению для апскейла
+        :param output_path:  путь к выходному файлу
+        :param model_path: путь к ИИ модели
+        :return:
+    """
+
+    try:
+        image = cv2.imread(input_path)
+    except FileNotFoundError as err:
+        print(err)
+        return ''
+
+    scaler = get_model(model_path)
     result = scaler.upsample(image)
-    cv2.imwrite(output_path, result)
+
+    try:
+        cv2.imwrite(output_path, result)
+    except OSError as err:
+        print(err)
+        return ''
+
+    return output_path
 
 
 def example():
-    upscale('example/lama_300px.png', 'lama_600px.png')
+    result_file = upscale(
+        os.path.join(ML_OBJECTS, 'lama_300px.png'),
+        os.path.join(ML_RESULTS, 'lama_600px.png')
+    )
+    print(f'Success\t{result_file}' if result_file else 'Sorry...')
 
 
 if __name__ == '__main__':
