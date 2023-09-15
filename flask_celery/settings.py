9aeab3e5ad66ce83444cb7d9348c5d4bf0174d5b
Sergey Kievskiy <knhel7@mail.ru>
2023-09-15 13:28:31 +0300

    Init

--- flask_celery/settings.py
+++ flask_celery/settings.py
@@ -3,11 +3,11 @@ import os
 
 load_dotenv()
 
-
 PATH = os.path.join(os.getcwd())
 
 CELERY_BROKER = os.getenv("CELERY_BROKER")
 CELERY_BACKEND = os.getenv("CELERY_BACKEND")
+CELERY_STORAGE = os.getenv("CELERY_STORAGE")
 
 ML_PACKAGE = os.getenv("ML_PACKAGE")
 ML_STORAGE = os.getenv("ML_STORAGE")
@@ -15,5 +15,3 @@ ML_EXAMPLES = os.getenv("ML_OBJECTS_FOLDER")
 ML_MODELS_FOLDER = os.getenv("ML_MODELS_FOLDER")
 ML_MODEL_NAME = os.getenv("ML_CURRENT_MODEL_NAME")
 ML_MODEL = os.path.join(ML_MODELS_FOLDER, ML_MODEL_NAME)
-
-STORAGE = os.path.join(ML_PACKAGE, ML_STORAGE)
