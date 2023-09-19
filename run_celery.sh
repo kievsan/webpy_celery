33975c36594a99a9e9e118c72c38b1d2103ef81e
Sergey Kievskiy <knhel7@mail.ru>
2023-09-19 10:54:55 +0300

    Init

--- run_celery.sh
+++ run_celery.sh
@@ -1,1 +1,1 @@
-celery -A flask_celery.celery_app.celery_app worker -c 2 -l info
+celery -A flask_celery.celery_app.celery_app -b "redis://127.0.0.1:6380/1" --result-backend "redis://127.0.0.1:6380/2" worker -c 2 -l info
