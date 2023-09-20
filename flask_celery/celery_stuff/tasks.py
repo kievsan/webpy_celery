86141c3a1deeb7988ace468c923e4fcaca043b86
Sergey Kievskiy <knhel7@mail.ru>
2023-09-20 09:18:47 +0300

    Init

--- flask_celery/celery_stuff/tasks.py
+++ flask_celery/celery_stuff/tasks.py
@@ -4,13 +4,17 @@ import flask_celery.upscale.upscale as upscale_image
 from flask_celery.celery_stuff.app import celery_app
 
 
-upscale_image_task = celery_app.task(upscale_image.upscale_on_server,
-                                     name='celery_tasks.upscale_image_task')
-upscale_example_task = celery_app.task(upscale_image.upscale_example,
-                                     name='celery_tasks.upscale_example_task')
+upscale_image_task = celery_app.task(
+    upscale_image.upscale_on_server,
+    name='celery_tasks.upscale_image'
+)
 
+upscale_example_task = celery_app.task(
+    upscale_image.upscale_example,
+    name='celery_tasks.upscale_example'
+)
 
-@celery_app.task(name='celery_tasks.simple_task')
+@celery_app.task(name='celery_tasks.simple')
 def simple_task(msg: str):
     print('START SIMPLE example')
     words = msg.split()
