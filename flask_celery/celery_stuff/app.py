b253ebb1c9195adcbbeeeafd7004766293c4a005
Sergey Kievskiy <knhel7@mail.ru>
2023-09-20 10:44:22 +0300

    Init

--- flask_celery/celery_stuff/app.py
+++ flask_celery/celery_stuff/app.py
@@ -1,14 +1,15 @@
 from celery import Celery
 from celery.result import AsyncResult
 
-from flask_celery.settings import CELERY_BROKER, CELERY_BACKEND
+# from flask_celery.settings import CELERY_BROKER, CELERY_BACKEND
 
 celery_app = Celery("flask_celery.celery_stuff.app",
                     backend="redis://127.0.0.1:6380/2",
                     broker="redis://127.0.0.1:6380/1",
-                    include=['flask_celery.celery_stuff.tasks'])
+                    include=['flask_celery.celery_stuff.tasks']
+                    )
                     # backend=CELERY_BACKEND,  # os.getenv('CELERY_BACKEND'),
-                    # broker=CELERY_BROKER)  # os.getenv('CELERY_BROKER'))
+                    # broker=CELERY_BROKER)  # os.getenv('CELERY_BROKER'),
 
 
 def get_task_result(task_id: str) -> AsyncResult:
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-PYTHONUNBUFFERED=TRUE gunicorn -b 0.0.0.0:5000 --capture-output flask_celery.app:flask_app
+PYTHONUNBUFFERED=TRUE gunicorn -b 0.0.0.0:5000 --capture-output flask_celery.server:flask_app
