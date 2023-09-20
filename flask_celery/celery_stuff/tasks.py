ddb0a4286346f99cc3660c2ea5cee586bc6f25af
Sergey Kievskiy <knhel7@mail.ru>
2023-09-20 11:31:37 +0300

    Init

--- flask_celery/celery_stuff/tasks.py
+++ flask_celery/celery_stuff/tasks.py
@@ -6,7 +6,8 @@ from flask_celery.celery_stuff.app import celery_app
 
 upscale_image_task = celery_app.task(
     upscale_image.upscale_on_server,
-    name='flask_celery.celery_stuff.tasks.upscale_image'
+    name='flask_celery.celery_stuff.tasks.upscale_image',
+    ignore_result=True
 )
 
 upscale_example_task = celery_app.task(
